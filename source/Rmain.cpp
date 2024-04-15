9fa49a59f15db1d6ea95e3b8ce1f6cb20232b517
kuznetsov-radio <51828521+kuznetsov-radio@users.noreply.github.com>
2024-04-15 13:12:45 +0800

    Add files via upload

--- source/Rmain.cpp
+++ source/Rmain.cpp
@@ -6,6 +6,7 @@
 #include "ExtMath.h"
 #include "RenderIrregular.h"
 #include "MWtransfer.h"
+#include "Messages.h"
 
 #ifndef LINUX
 #include <ppl.h>
@@ -275,7 +276,7 @@ extern "C" double ComputeMW_fragment(int argc, void **argv)
  if (DEM_on || DDM_on)
  {
   LgridL=(double*)malloc(e_NL*sizeof(double));
-  for (int j=0; j<e_NL; j++) LgridL[j]=log(e_Lrun[D2(e_NQ, 0, j)]);
+  for (int j=0; j<e_NL; j++) LgridL[j]=log((double)e_Lrun[D2(e_NQ, 0, j)]);
 
   QgridL=(double*)malloc(e_NQ*e_NL*sizeof(double));
   for (int i=0; i<e_NQ*e_NL; i++) QgridL[i]=log((double)e_Qrun[i]); //### - possibly, transpose
@@ -657,4 +658,91 @@ extern "C" int pyComputeMW(void* model, void* ebtel, void* simbox, void* cparms,
  ARGV[4]=out;
 
  return ComputeMW(5, ARGV);
+}
+
+#ifndef LINUX
+extern "C" __declspec(dllexport) int InterpolateEBTEL(int argc, void **argv)
+#else
+extern "C" int InterpolateEBTEL(int argc, void **argv)
+#endif
+{
+ int *Lparms=(int*)argv[0];
+ int Npoints=Lparms[0];
+ int NQ=Lparms[1];
+ int NL=Lparms[2];
+ int NT=Lparms[3];
+ int DEM_on=Lparms[4];
+ int DDM_on=Lparms[5];
+
+ float *Qrun=(float*)argv[1];
+ float *Lrun=(float*)argv[2];
+ float *DEM_run=(float*)argv[3];
+ float *DDM_run=(float*)argv[4];
+
+ double *Qarr=(double*)argv[5];
+ double *Larr=(double*)argv[6];
+
+ double *DEM_arr=(double*)argv[7];
+ double *DDM_arr=(double*)argv[8];
+ char *flag=(char*)argv[9];
+
+ double *LgridL=(double*)malloc(NL*sizeof(double));
+ for (int j=0; j<NL; j++) LgridL[j]=log((double)Lrun[D2(NQ, 0, j)]);
+
+ double *QgridL=(double*)malloc(NQ*NL*sizeof(double));
+ for (int i=0; i<NQ*NL; i++) QgridL[i]=log((double)Qrun[i]);
+
+ #ifndef LINUX
+ concurrency::parallel_for(0, Npoints, [&](int k)
+ {
+ #else
+ #pragma omp parallel for
+ for (int k=0; k<Npoints; k++)
+ {
+ #endif
+  flag[k]=0;
+
+  double Qlog=log(Qarr[k]);
+  double Llog=log(Larr[k]);
+
+  int Lind=value_locate(LgridL, NL, Llog);
+
+  if (Lind>=0 && Lind<(NL-1))
+  {
+   int Qind1=value_locate(QgridL+Lind*NQ, NQ, Qlog);
+   int Qind2=value_locate(QgridL+(Lind+1)*NQ, NQ, Qlog);
+
+   if (Qind1>=0 && Qind1<(NQ-1) && Qind2>=0 && Qind2<(NQ-1))
+   {
+	flag[k]=1;
+
+	double dL=(Llog-LgridL[Lind])/(LgridL[Lind+1]-LgridL[Lind]);
+    double dQ1=(Qlog-QgridL[D2(NQ, Qind1, Lind)])/(QgridL[D2(NQ, Qind1+1, Lind)]-QgridL[D2(NQ, Qind1, Lind)]);
+    double dQ2=(Qlog-QgridL[D2(NQ, Qind2, Lind+1)])/(QgridL[D2(NQ, Qind2+1, Lind+1)]-QgridL[D2(NQ, Qind2, Lind+1)]);
+
+	if (DEM_on)
+	 for (int l=0; l<NT; l++)
+	  DEM_arr[D2(NT, l, k)]=DEM_run[D3(NT, NQ, l, Qind1, Lind)]*(1.0-dL)*(1.0-dQ1)+
+                            DEM_run[D3(NT, NQ, l, Qind1+1, Lind)]*(1.0-dL)*dQ1+
+                            DEM_run[D3(NT, NQ, l, Qind2, Lind+1)]*dL*(1.0-dQ2)+
+                            DEM_run[D3(NT, NQ, l, Qind2+1, Lind+1)]*dL*dQ2;
+
+	if (DDM_on)
+ 	 for (int l=0; l<NT; l++)
+	  DDM_arr[D2(NT, l, k)]=DDM_run[D3(NT, NQ, l, Qind1, Lind)]*(1.0-dL)*(1.0-dQ1)+
+                            DDM_run[D3(NT, NQ, l, Qind1+1, Lind)]*(1.0-dL)*dQ1+
+                            DDM_run[D3(NT, NQ, l, Qind2, Lind+1)]*dL*(1.0-dQ2)+
+                            DDM_run[D3(NT, NQ, l, Qind2+1, Lind+1)]*dL*dQ2; 
+   }
+  }
+ #ifndef LINUX
+ });
+ #else
+ }
+ #endif
+
+ free(LgridL);
+ free(QgridL);
+
+ return 0;
 }
